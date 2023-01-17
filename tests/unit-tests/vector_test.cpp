6b2500884a89ba57d16de027c6288e885e941c07
nori <nori623p@gmail.com>
2023-01-17 13:37:40 +0100

    test: 💍 vector デフォルトコンストラクタとreserveのテスト追加

--- tests/unit-tests/vector_test.cpp
+++ tests/unit-tests/vector_test.cpp
@@ -12,9 +12,28 @@ TEST(vector, example)
 	EXPECT_TRUE(true);
 }
 
+// If size() is ​0​, data() may or may not return a null pointer.
 TEST(vector, default_construtor)
 {
-	ft::vector<int> i = ft::vector<int>();
-	EXPECT_EQ(i.size(), 0U);
-	EXPECT_EQ(i.capacity(), 0U);
+	ft::vector<int> v = ft::vector<int>();
+	EXPECT_EQ(v.size(), 0U);
+	EXPECT_EQ(v.capacity(), 0U);
+	EXPECT_EQ(v.data(), (int *)0);
+	EXPECT_TRUE(v.empty());
+}
+
+TEST(vector, reserve)
+{
+	ft::vector<int> v = ft::vector<int>();
+	v.reserve(0);
+	EXPECT_EQ(v.size(), 0U);
+	EXPECT_EQ(v.capacity(), 0U);
+	EXPECT_EQ(v.data(), (int *)0);
+	EXPECT_TRUE(v.empty());
+
+	v.reserve(1);
+	EXPECT_EQ(v.size(), 0U);
+	EXPECT_EQ(v.capacity(), 1U);
+	EXPECT_NE(v.data(), (int *)0);
+	EXPECT_TRUE(v.empty());
 }
