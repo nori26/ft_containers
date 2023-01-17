e2090e03bdb3764af108d4f10664d35091d8e31b
nori <nori623p@gmail.com>
2023-01-17 13:39:05 +0100

    feat: 🎸 デフォルトコンストラクタ実装

--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -1,6 +1,23 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
-namespace ft {}
+#include <memory>
+
+namespace ft
+{
+	template <class T, class Allocator = std::allocator<T> >
+	class vector
+	{
+		typedef T *pointer;
+
+		pointer   front_;
+		pointer   back_;
+		pointer   reserved_last_;
+		Allocator allocator_;
+
+	  public:
+		vector() : front_(NULL), back_(NULL), reserved_last_(NULL), allocator_(std::allocator<T>()) {}
+	};
+} // namespace ft
 
 #endif
