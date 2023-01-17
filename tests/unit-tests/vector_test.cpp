9ea0907367bfd7f555397d61fe51a6392f19e5c2
nori <nori623p@gmail.com>
2023-01-17 04:59:54 +0100

    test: 💍 最初はstlの動作見たい気がしたのでエディタ上のデフォルトをstlにした

--- tests/unit-tests/vector_test.cpp
+++ tests/unit-tests/vector_test.cpp
@@ -1,13 +1,20 @@
 #include "gtest.h"
 
-#ifdef STL_TEST
+#ifdef FT_TEST
+  #include "vector.hpp"
+#else
   #include <vector>
 namespace ft = std;
-#else
-  #include "vector.hpp"
 #endif
 
 TEST(vector, example)
 {
 	EXPECT_TRUE(true);
+}
+
+TEST(vector, default_construtor)
+{
+	ft::vector<int> i = ft::vector<int>();
+	EXPECT_EQ(i.size(), 0U);
+	EXPECT_EQ(i.capacity(), 0U);
 }
