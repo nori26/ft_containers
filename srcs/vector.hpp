6db258f6ec1996497f390a3074e75a14c3897319
nori <nori623p@gmail.com>
2023-01-24 02:04:58 +0100

    refactor: 💡 typedefをallocator_traitsに合わせて修正

--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -12,15 +12,15 @@ namespace ft
 		// TODO allocatorから？
 	  public:
 		typedef Allocator                                allocator_type;
-		typedef typename allocator_type::value_type      value_type;
-		typedef typename allocator_type::size_type       size_type;
-		typedef typename allocator_type::difference_type difference_type;
 		typedef typename allocator_type::pointer         pointer;
 		typedef typename allocator_type::const_pointer   const_pointer;
-		typedef value_type                              &reference;
-		typedef const value_type                        &const_reference;
-		typedef typename allocator_type::pointer         iterator;
-		typedef const value_type                        *const_iterator;
+		typedef typename allocator_type::value_type      value_type;
+		typedef typename allocator_type::reference       reference;
+		typedef typename allocator_type::const_reference const_reference;
+		typedef typename allocator_type::size_type       size_type;
+		typedef typename allocator_type::difference_type difference_type;
+		typedef pointer                                  iterator;
+		typedef const_pointer                            const_iterator;
 
 		pointer   first_;
 		pointer   last_;
