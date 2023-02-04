cbc75b0540e054b8e0515799e9536b0b53a43ba5
nori <nori623p@gmail.com>
2023-02-04 15:28:55 +0100

    test: 💍 erase

---
+++ tests/unit-tests/vector/erase.cpp
@@ -0,0 +1,125 @@
+#include "gtest.h"
+
+#include "allocator.hpp"
+#include "data.hpp"
+#include "init_vector.hpp"
+
+#ifdef FT_TEST
+  #include "vector.hpp"
+#else
+  #include <vector>
+namespace ft = std;
+#endif
+
+namespace ftc = ft_containers;
+typedef ft::vector<ftc::Data>                             Vector;
+typedef ft::vector<ftc::Data, ftc::Allocator<ftc::Data> > VectorAl;
+
+#define ARRAY_SIZE(ary) (sizeof(ary) / sizeof(ary[0]))
+
+TEST(vector, erase_forward)
+{
+	size_t    a[]   = {2, 3};
+	ftc::Data res[] = {3};
+	Vector    v;
+
+	ftc::initv(v, a, a + ARRAY_SIZE(a));
+	ftc::Data        *p   = v.data();
+	Vector::size_type cap = v.capacity();
+	Vector::iterator  it  = v.erase(v.begin());
+	EXPECT_EQ(it, v.begin());
+	EXPECT_EQ(v.data(), p);
+	EXPECT_EQ(v.capacity(), cap);
+	EXPECT_EQ(v.size(), ARRAY_SIZE(res));
+	for (size_t i = 0; i < ARRAY_SIZE(res); i++) {
+		EXPECT_EQ(v[i], res[i]);
+	}
+}
+
+TEST(vector, erase_forward2)
+{
+	size_t    a[]   = {2, 3, 4};
+	ftc::Data res[] = {3, 4};
+	Vector    v;
+
+	ftc::initv(v, a, a + ARRAY_SIZE(a));
+	ftc::Data        *p   = v.data();
+	Vector::size_type cap = v.capacity();
+	Vector::iterator  it  = v.erase(v.begin());
+	EXPECT_EQ(it, v.begin());
+	EXPECT_EQ(v.data(), p);
+	EXPECT_EQ(v.capacity(), cap);
+	EXPECT_EQ(v.size(), ARRAY_SIZE(res));
+	for (size_t i = 0; i < ARRAY_SIZE(res); i++) {
+		EXPECT_EQ(v[i], res[i]);
+	}
+}
+
+TEST(vector, erase_forward3)
+{
+	size_t    a[129]   = {2, 3, 4, 5};
+	ftc::Data res[128] = {3, 4, 5};
+	Vector    v;
+
+	ftc::initv(v, a, a + ARRAY_SIZE(a));
+	ftc::Data        *p   = v.data();
+	Vector::size_type cap = v.capacity();
+	Vector::iterator  it  = v.erase(v.begin());
+	EXPECT_EQ(it, v.begin());
+	EXPECT_EQ(v.data(), p);
+	EXPECT_EQ(v.capacity(), cap);
+	EXPECT_EQ(v.size(), ARRAY_SIZE(res));
+	for (size_t i = 0; i < ARRAY_SIZE(res); i++) {
+		ASSERT_EQ(v[i], res[i]);
+	}
+}
+
+TEST(vector, erase_forward4)
+{
+	size_t a[] = {1};
+	Vector v;
+
+	ftc::initv(v, a, a + ARRAY_SIZE(a));
+	ftc::Data        *p   = v.data();
+	Vector::size_type cap = v.capacity();
+	Vector::iterator  it  = v.erase(v.begin());
+	EXPECT_EQ(it, v.begin());
+	EXPECT_EQ(v.data(), p);
+	EXPECT_EQ(v.capacity(), cap);
+	EXPECT_EQ(v.size(), 0U);
+}
+
+TEST(vector, erase_backward)
+{
+	size_t a[] = {1};
+	Vector v;
+
+	ftc::initv(v, a, a + ARRAY_SIZE(a));
+	ftc::Data        *p   = v.data();
+	Vector::size_type cap = v.capacity();
+	Vector::iterator  it  = v.erase(v.end() - 1);
+	EXPECT_EQ(it, v.end());
+	EXPECT_EQ(v.data(), p);
+	EXPECT_EQ(v.capacity(), cap);
+	EXPECT_EQ(v.size(), 0U);
+}
+
+TEST(vector, erase_backward2)
+{
+	size_t    a[]   = {2, 3};
+	ftc::Data res[] = {2};
+	Vector    v;
+
+	ftc::initv(v, a, a + ARRAY_SIZE(a));
+	ftc::Data        *p   = v.data();
+	Vector::size_type cap = v.capacity();
+	Vector::iterator  it  = v.erase(v.end() - 1);
+	EXPECT_EQ(it, v.end());
+	EXPECT_EQ(v.data(), p);
+	EXPECT_EQ(v.capacity(), cap);
+	EXPECT_EQ(v.size(), ARRAY_SIZE(res));
+	for (size_t i = 0; i < ARRAY_SIZE(res); i++) {
+		EXPECT_EQ(v[i], res[i]);
+	}
+}
+
