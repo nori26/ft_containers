bb5265f4b6b6f8955fb692c85f94d44b0ad156d5
nori <nori623p@gmail.com>
2023-02-11 11:00:00 +0100

    test: 💍 copy constructor

--- tests/unit-tests/vector/constructor.cpp
+++ tests/unit-tests/vector/constructor.cpp
@@ -39,25 +39,85 @@ TEST_F(vector, constructor_alloc)
 	EXPECT_EQ(v[0], 1);
 }
 
+TEST_F(vector, copy_constructor_empty)
+{
+	Vector v;
+	Vector v2 = v;
+
+	EXPECT_EQ(v2.size(), 0U);
+	EXPECT_EQ(v2.capacity(), 0U);
+	EXPECT_EQ(v2.data(), (ftc::Data *)NULL);
+	EXPECT_EQ(v.get_allocator().get_id(), v2.get_allocator().get_id());
+}
+
 TEST_F(vector, copy_constructor)
 {
-	{
-		Vector v;
+	Vector v;
+	size_t cap = 128;
 
-		v.reserve(128);
-		for (size_t i = 0; i < 128; i++) {
-			v.resize(i);
-			Vector v2 = Vector(v);
-			ASSERT_EQ(v2.size(), i);
-			ASSERT_EQ(v2.capacity(), i);
-		}
+	v.reserve(cap);
+	for (size_t i = 0; i < cap; i++) {
+		v.resize(i);
+		Vector v2 = Vector(v);
+		EXPECT_NE(v.data(), v2.data());
+		ASSERT_EQ(v.size(), i);
+		ASSERT_EQ(v.capacity(), cap);
+		ASSERT_EQ(v2.size(), i);
+		ASSERT_EQ(v2.capacity(), i);
+		ASSERT_EQ(v.get_allocator().get_id(), v2.get_allocator().get_id());
 	}
-	{
-		Vector v1;
-		v1.push_back(1);
+}
 
-		Vector v2 = v1;
-		v1[0]     = 0;
-		EXPECT_NE(v1[0], v2[0]);
+TEST_F(vector, copy_constructor2)
+{
+	Vector v1;
+	v1.push_back(1);
+
+	Vector v2 = v1;
+	v1[0]     = 0;
+	EXPECT_NE(v1[0], v2[0]);
+	EXPECT_NE(v1.data(), v2.data());
+	EXPECT_EQ(v1.get_allocator().get_id(), v2.get_allocator().get_id());
+}
+
+TEST_F(vector, copy_constructor3)
+{
+	ftc::Data a1[] = {1, 2, 3};
+	Vector    v1;
+
+	ftc::initv(v1, a1, a1 + ARRAY_SIZE(a1));
+	Vector v2(v1);
+	for (size_t i = 0; i < ARRAY_SIZE(a1); i++) {
+		ASSERT_EQ(v1[i], a1[i]);
 	}
+	for (size_t i = 0; i < ARRAY_SIZE(a1); i++) {
+		ASSERT_EQ(v2[i], a1[i]);
+	}
+	EXPECT_NE(v1.data(), v2.data());
+	EXPECT_EQ(v1.size(), ARRAY_SIZE(a1));
+	EXPECT_EQ(v1.capacity(), ARRAY_SIZE(a1));
+	EXPECT_EQ(v2.size(), ARRAY_SIZE(a1));
+	EXPECT_EQ(v2.capacity(), ARRAY_SIZE(a1));
+	EXPECT_EQ(v1.get_allocator().get_id(), v2.get_allocator().get_id());
+}
+
+TEST_F(vector, copy_constructor4)
+{
+	ftc::Data a1[256] = {1, 2, 3};
+	Vector    v1;
+
+	ftc::initv(v1, a1, a1 + ARRAY_SIZE(a1));
+	Vector v2(v1);
+	for (size_t i = 0; i < ARRAY_SIZE(a1); i++) {
+		ASSERT_EQ(v1[i], a1[i]);
+	}
+	for (size_t i = 0; i < ARRAY_SIZE(a1); i++) {
+		ASSERT_EQ(v2[i], a1[i]);
+	}
+	EXPECT_NE(v1.data(), v2.data());
+	EXPECT_EQ(v1.size(), ARRAY_SIZE(a1));
+	EXPECT_EQ(v1.capacity(), ARRAY_SIZE(a1));
+	EXPECT_EQ(v2.size(), ARRAY_SIZE(a1));
+	EXPECT_EQ(v2.capacity(), ARRAY_SIZE(a1));
+	EXPECT_EQ(v1.get_allocator().get_id(), v2.get_allocator().get_id());
 }
