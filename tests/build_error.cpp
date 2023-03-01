156fd5181b95526928abecc95ac878baa1b3ba28
nori <nori623p@gmail.com>
2023-03-01 11:08:32 +0100

    test: 💍 コンパイルエラーになるべきケースのメモ

---
+++ tests/build_error.cpp
@@ -0,0 +1,30 @@
+#include "gtest.h"
+
+#include "allocator.hpp"
+#include "data.hpp"
+
+#ifdef FT_TEST
+  #include "vector.hpp"
+#else
+  #include <vector>
+namespace ft = std;
+#endif
+
+namespace ftc = ft_containers;
+typedef ft::vector<ftc::Data, ftc::Allocator<ftc::Data> > Vector;
+typedef ftc::Allocator<ftc::Data>::ExceptionOn            AllocExceptionOn;
+
+#define ARRAY_SIZE(ary) (sizeof(ary) / sizeof(ary[0]))
+
+int main()
+{
+	// Vector::iterator it;
+	// (void)(it + it);
+	// (void)(1 - it);
+
+	// Vector::iterator it = Vector::iterator(); //set but not used
+
+	// ft::vector<char>       v;
+	// ft::vector<const char> v2;
+	// v == v2;
+}
