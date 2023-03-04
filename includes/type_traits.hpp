355f38e7b899f2010c6d073d09b2bcef034bbf93
nori <nori623p@gmail.com>
2023-03-04 12:27:28 +0100

    feat: 🎸 is_convertible

--- includes/type_traits.hpp
+++ includes/type_traits.hpp
@@ -13,7 +13,29 @@ namespace ft
 		typedef T type;
 	};
 
+	template <typename From, typename To>
+	struct is_convertible
+	{
+	  private:
+		typedef struct
+		{
+			char a[2];
+		} no;
+		typedef char yes;
 
+		static From f;
+		static yes  test(To);
+		static no   test(...);
+
+	  public:
+	  	// sizeofはオペランドの式を評価しない
+		static const bool value = sizeof(test(f)) == sizeof(yes);
+	};
+	template <typename From, typename To>
+	From is_convertible<From, To>::f;
+
+	template <typename From, typename To>
+	const bool is_convertible<From, To>::value;
 } // namespace ft
 
 #endif
