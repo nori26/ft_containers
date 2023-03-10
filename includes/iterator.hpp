e1f7c6ce22433a307a34422bb55e0bc0f9450b25
nori <nori623p@gmail.com>
2023-03-10 07:28:57 +0100

    feat: 🎸 wip reverse_iterator

--- includes/iterator.hpp
+++ includes/iterator.hpp
@@ -2,5 +2,6 @@
 #define ITERATOR_HPP
 
 #include "iterator/iterator_traits.hpp"
+#include "iterator/reverse_iterator.hpp"
 
 #endif
---
+++ includes/iterator/reverse_iterator.hpp
@@ -0,0 +1,31 @@
+#ifndef REVERSE_ITERATOR_HPP
+#define REVERSE_ITERATOR_HPP
+
+#include <iterator>
+
+#include "iterator.hpp"
+
+namespace ft
+{
+	template <typename Iter>
+	class reverse_iterator : public std::iterator<
+								 typename iterator_traits<Iter>::iterator_category,
+								 typename iterator_traits<Iter>::value_type,
+								 typename iterator_traits<Iter>::difference_type,
+								 typename iterator_traits<Iter>::pointer,
+								 typename iterator_traits<Iter>::reference>
+	{
+	  protected:
+		Iter current;
+
+	  public:
+		reverse_iterator() : current() {}
+
+		template <typename T>
+		reverse_iterator(const reverse_iterator<T> &other) : current()
+		{
+			(void)other;
+		}
+	};
+} // namespace ft
+#endif
