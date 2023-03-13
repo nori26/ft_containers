4407dc09f509959f6fb9a58b2dd0353c4fda348b
nori <nori623p@gmail.com>
2023-03-13 02:14:01 +0100

    feat: 🎸 wip is_integral、integral constant未継承

--- includes/type_traits.hpp
+++ includes/type_traits.hpp
@@ -59,6 +59,61 @@ namespace ft
 	{
 		typedef T type;
 	};
+
+	template <typename T>
+	struct remove_const
+	{
+		typedef T type;
+	};
+
+	template <typename T>
+	struct remove_const<const T>
+	{
+		typedef T type;
+	};
+
+	template <typename T>
+	struct remove_volatile
+	{
+		typedef T type;
+	};
+
+	template <typename T>
+	struct remove_volatile<volatile T>
+	{
+		typedef T type;
+	};
+
+	template <typename T>
+	struct remove_cv
+	{
+	  private:
+		typedef typename remove_volatile<T>::type non_volatile_type;
+
+	  public:
+		typedef typename remove_const<non_volatile_type>::type type;
+	};
+
+	// clang-format off
+	// long long は C++11
+	template <typename>
+	struct is_integral_base : public false_type {};
+
+	template <> struct is_integral_base<bool>           : public true_type {};
+	template <> struct is_integral_base<char>           : public true_type {};
+	template <> struct is_integral_base<wchar_t>        : public true_type {};
+	template <> struct is_integral_base<short>          : public true_type {};
+	template <> struct is_integral_base<int>            : public true_type {};
+	template <> struct is_integral_base<long>           : public true_type {};
+	template <> struct is_integral_base<signed char>    : public true_type {};
+	template <> struct is_integral_base<unsigned char>  : public true_type {};
+	template <> struct is_integral_base<unsigned short> : public true_type {};
+	template <> struct is_integral_base<unsigned int>   : public true_type {};
+	template <> struct is_integral_base<unsigned long>  : public true_type {};
+
+	template <typename T>
+	struct is_integral : is_integral_base<typename remove_cv<T>::type> {};
+	// clang-format on
 } // namespace ft
 
 #endif
