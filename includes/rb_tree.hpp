d2cba825ea62eb9afe5a6d09befa608a411ea57d
nori <nori623p@gmail.com>
2023-03-21 10:38:00 +0100

    feat: 🎸 rb_tree::insert大体できた

--- includes/rb_tree.hpp
+++ includes/rb_tree.hpp
@@ -32,18 +32,24 @@ namespace ft
 	template <typename T, typename U>
 	class rb_tree
 	{
-	  private:
+		// protected for visualize
+	  protected:
 		typedef rb_tree_node<T, U> node_type;
 
 	  public:
 		typedef T key_type;
 		typedef U value_type;
 
+	  protected:
+		node_type *root_;
+
 	  private:
-		node_type                *root_;
-		std::allocator<node_type> allocator_;
+		std::allocator<node_type> allocator_; // TODO tempalte
+		bool                      is_balanced_;
 
 	  public:
+		rb_tree() : root_(), is_balanced_(true) {}
+
 		// TODO 例外安全
 		~rb_tree()
 		{
@@ -65,29 +71,100 @@ namespace ft
 
 		void insert(const T &key, const U &value)
 		{
-			insert_recursive(NULL, root_, key, value);
+			is_balanced_ = true;
+			root_        = insert_recursive(NULL, root_, key, value);
 			root_->color = node_type::BLACK;
 		}
 
 	  private:
-		void insert_recursive(node_type *parent, node_type *&node, const T &key, const U &value)
+		node_type *
+		insert_recursive(node_type *parent, node_type *&node, const T &key, const U &value)
 		{
 			if (node == NULL) {
-				node = new_node(parent, key, value);
+				is_balanced_ = false;
+				return new_node(parent, key, value);
 			} else if (key > node->key) {
-				insert_recursive(node, node->right, key, value);
+				node->right = insert_recursive(node, node->right, key, value);
+				return balance_insert(node);
 			} else if (key < node->key) {
-				insert_recursive(node, node->left, key, value);
+				node->left = insert_recursive(node, node->left, key, value);
+				return balance_insert(node);
 			} else {
 				node->value = value;
+				return node;
 			}
 		}
 
 		node_type *new_node(node_type *parent, const T &key, const U &value)
 		{
 			node_type *p = allocator_.allocate(1);
-			*p           = node_type(parent, key, value);
+			allocator_.construct(p, node_type(parent, key, value));
 			return p;
+		}
+
+		node_type *balance_insert(node_type *top)
+		{
+			if (is_balanced_ == true || top == NULL || is_red(top)) {
+				return top;
+			}
+			/*
+				   2B          2R
+				   / \         / \
+				 1R   3R  => 1B   3B
+				 /           /
+				0R          2R
+			*/
+			if (is_red(top->left) && is_red(top->right) &&
+				(has_red_child(top->left) || has_red_child(top->right))) {
+				top->color        = node_type::RED;
+				top->left->color  = node_type::BLACK;
+				top->right->color = node_type::BLACK;
+				return top;
+			}
+			/*
+				  2B            2B
+				  / \           / \
+				0R   3B  =>   1R   3B
+				 \           /
+				  1R       0R
+			*/
+			if (is_red(top->left) && is_red(top->left->right)) {
+				top->left = rotate_left(top->left);
+			} else if (is_red(top->right) && is_red(top->right->left)) {
+				top->right = rotate_left(top->right);
+			}
+			/*
+				   2B         1R           1B
+				   / \        / \          / \
+				 1R   3B => 0R   2B   => 0R   2R
+				 /                \            \
+				0R                 3B           3B
+			*/
+			if (is_red(top->left) && is_red(top->left->left)) {
+				top               = rotate_right(top);
+				top->right->color = node_type::RED;
+			} else if (is_red(top->right) && is_red(top->right->right)) {
+				top              = rotate_left(top);
+				top->left->color = node_type::RED;
+			}
+			top->color   = node_type::BLACK;
+			is_balanced_ = true;
+			return top;
+		}
+
+		bool has_red_child(node_type *n)
+		{
+			return is_red(n->left) || is_red(n->right);
+		}
+
+		bool is_black(node_type *n)
+		{
+			return n == NULL || n->color == node_type::BLACK;
+		}
+
+		bool is_red(node_type *n)
+		{
+			return !is_black(n);
 		}
 
 	  public:
@@ -100,45 +177,57 @@ namespace ft
 			size_t                  prev_height = 0;
 			std::queue<node_type *> q;
 
-			q.push(root_);
-			while (!q.empty()) {
-				node_type *n = q.front();
-				height       = get_height(n);
-				if (height != prev_height) {
-					std::cout << std::endl;
-				}
-				prev_height = height;
-				switch (n->color) {
-				case node_type::RED:
-					std::cout << "\033[1;31m";
-					break;
-				case node_type::BLACK:
-					break;
-				default:
-					std::cerr << "invalid color" << std::endl;
-					abort();
-				}
-				std::cout << n->key << " "
-						  << "\033[0m";
-				if (n->left != NULL) {
-					q.push(n->left);
-				}
-				if (n->right != NULL) {
-					q.push(n->right);
-				}
-				q.pop();
+		node_type *rotate_left(node_type *n)
+		{
+			if (n == NULL || n->right == NULL) {
+				return n;
 			}
-			std::cout << std::endl;
+			node_type *parent = n->parent;
+			node_type *right  = n->right;
+			n->parent         = right;
+			n->right          = right->left;
+			if (n->right) {
+				n->right->parent = n;
+			}
+			right->parent = parent;
+			right->left   = n;
+			return right;
 		}
 
-	  private:
-		size_t get_height(node_type *n)
+		node_type *rotate_right(node_type *n)
 		{
-			size_t i = 0;
-			for (; n->parent; n = n->parent) {
-				i++;
+			if (n == NULL || n->left == NULL) {
+				return n;
 			}
-			return i;
+			node_type *parent = n->parent;
+			node_type *left   = n->left;
+			n->parent         = left;
+			n->left           = left->right;
+			if (n->left) {
+				n->left->parent = n;
+			}
+			left->parent = parent;
+			left->right  = n;
+			return left;
+		}
+
+		node_type *double_rotate_left(node_type *n)
+		{
+			n->right = rotate_right(n->right);
+			return rotate_left(n);
+		}
+
+		node_type *double_rotate_right(node_type *n)
+		{
+			n->left = rotate_left(n->left);
+			return rotate_right(n);
+		}
+
+		void unlink(node_type *n)
+		{
+			n->parent = NULL;
+			n->left   = NULL;
+			n->right  = NULL;
 		}
 	};
 } // namespace ft
