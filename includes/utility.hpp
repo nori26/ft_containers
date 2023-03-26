7529d890afc5b4d7614d53702b915f430e77b898
nori <nori623p@gmail.com>
2023-03-26 16:31:05 +0200

    feat: 🎸 pair operator

--- includes/utility.hpp
+++ includes/utility.hpp
@@ -19,6 +19,16 @@ namespace ft
 		template <typename U1, typename U2>
 		pair(const pair<U1, U2> &p) : first(p.first), second(p.second)
 		{}
+
+		pair &operator=(const pair &rhs)
+		{
+			if (&rhs == this) {
+				return *this;
+			}
+			first  = rhs.first;
+			second = rhs.second;
+			return *this;
+		}
 	};
 
 	template <typename T1, typename T2>
@@ -31,6 +41,30 @@ namespace ft
 	bool operator!=(const pair<T1, T2> &a, const pair<T1, T2> &b)
 	{
 		return !(a == b);
+	}
+
+	template <typename T1, typename T2>
+	bool operator<(const pair<T1, T2> &a, const pair<T1, T2> &b)
+	{
+		return a.first < b.first || (!(b.first < a.first) && a.second < b.second);
+	}
+
+	template <typename T1, typename T2>
+	bool operator>(const pair<T1, T2> &a, const pair<T1, T2> &b)
+	{
+		return b < a;
+	}
+
+	template <typename T1, typename T2>
+	bool operator<=(const pair<T1, T2> &a, const pair<T1, T2> &b)
+	{
+		return !(b < a);
+	}
+
+	template <typename T1, typename T2>
+	bool operator>=(const pair<T1, T2> &a, const pair<T1, T2> &b)
+	{
+		return !(a < b);
 	}
 
 	// パラメータが参照型の場合、配列がポインタに変換されない
