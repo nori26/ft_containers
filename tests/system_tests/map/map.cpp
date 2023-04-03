de5c2141a6c1f3221407583cb5f0c3c07c696b1d
nori <nori623p@gmail.com>
2023-04-03 07:44:07 +0200

    test: 💍 map::key_comp, value_comp

--- tests/system_tests/map/map.cpp
+++ tests/system_tests/map/map.cpp
@@ -85,16 +85,198 @@ struct DerivedValCmp : public ft::map<int, int>::value_compare
 
 TEST(map, value_compare_types)
 {
+	Map m;
 
-	EXPECT_EQ(typeid(ft::map<int, long>::value_compare::first_argument_type), typeid(ft::pair<const int, long>));
-	EXPECT_EQ(typeid(ft::map<int, long>::value_compare::second_argument_type), typeid(ft::pair<const int, long>));
+	const Map::value_compare &c1 = m.value_comp();
+	const Map::value_compare &c2 = m.value_comp();
+
+	EXPECT_NE(&c1, &c2);
+	EXPECT_EQ(typeid(m.value_comp()), typeid(Map::value_compare));
+
+	EXPECT_EQ(
+		typeid(ft::map<int, long>::value_compare::first_argument_type),
+		typeid(ft::pair<const int, long>)
+	);
+	EXPECT_EQ(
+		typeid(ft::map<int, long>::value_compare::second_argument_type),
+		typeid(ft::pair<const int, long>)
+	);
 	EXPECT_EQ(typeid(ft::map<int, long>::value_compare::result_type), typeid(bool));
 }
 
-TEST(map, value_compare_comp)
+TEST(map, value_compare_types_const)
 {
-	DerivedValCmp                 cmp;
-	std::binder1st<DerivedValCmp> f = std::bind1st(cmp, ft::make_pair(1, 1));
+	const Map m;
 
-	EXPECT_TRUE(f(ft::make_pair(2, 2)));
+	const Map::value_compare &c1 = m.value_comp();
+	const Map::value_compare &c2 = m.value_comp();
+
+	EXPECT_NE(&c1, &c2);
+	EXPECT_EQ(typeid(m.value_comp()), typeid(Map::value_compare));
+
+	EXPECT_EQ(
+		typeid(ft::map<int, long>::value_compare::first_argument_type),
+		typeid(ft::pair<const int, long>)
+	);
+	EXPECT_EQ(
+		typeid(ft::map<int, long>::value_compare::second_argument_type),
+		typeid(ft::pair<const int, long>)
+	);
+	EXPECT_EQ(typeid(ft::map<int, long>::value_compare::result_type), typeid(bool));
+}
+
+TEST(map, value_compare_derived)
+{
+	DerivedValCmp cmp;
+
+	EXPECT_TRUE(cmp(ft::make_pair(1, 2), ft::make_pair(2, 2)));
+	EXPECT_FALSE(cmp(ft::make_pair(2, 2), ft::make_pair(2, 2)));
+	EXPECT_FALSE(cmp(ft::make_pair(2, 2), ft::make_pair(2, 3)));
+	EXPECT_FALSE(cmp(ft::make_pair(2, 1), ft::make_pair(1, 2)));
+	EXPECT_FALSE(cmp(ft::make_pair(2, 1), ft::make_pair(2, 0)));
+}
+
+TEST(map, value_compare)
+{
+	Map m;
+
+	EXPECT_TRUE(m.value_comp()(ft::make_pair(1, 2), ft::make_pair(2, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 2), ft::make_pair(2, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 2), ft::make_pair(2, 3)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 1), ft::make_pair(1, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 1), ft::make_pair(2, 0)));
+}
+
+TEST(map, value_compare_const)
+{
+	Map m;
+
+	EXPECT_TRUE(m.value_comp()(ft::make_pair(1, 2), ft::make_pair(2, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 2), ft::make_pair(2, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 2), ft::make_pair(2, 3)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 1), ft::make_pair(1, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 1), ft::make_pair(2, 0)));
+}
+
+template <typename Key>
+struct RevCmp
+{
+	bool operator()(const Key &a, const Key &b) const
+	{
+		return b < a;
+	}
+};
+
+TEST(map, value_compare_rev)
+{
+	typedef ft::map<ftc::Data, ftc::Data, RevCmp<ftc::Data>, ftc::Allocator<ValueType> > Map2;
+
+	Map2 m;
+
+	EXPECT_TRUE(m.value_comp()(ft::make_pair(2, 2), ft::make_pair(1, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 2), ft::make_pair(2, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 3), ft::make_pair(2, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(1, 2), ft::make_pair(2, 1)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 0), ft::make_pair(2, 1)));
+}
+
+TEST(map, value_compare_rev_const)
+{
+	typedef ft::map<ftc::Data, ftc::Data, RevCmp<ftc::Data>, ftc::Allocator<ValueType> > Map2;
+
+	const Map2 m;
+
+	EXPECT_TRUE(m.value_comp()(ft::make_pair(2, 2), ft::make_pair(1, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 2), ft::make_pair(2, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 3), ft::make_pair(2, 2)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(1, 2), ft::make_pair(2, 1)));
+	EXPECT_FALSE(m.value_comp()(ft::make_pair(2, 0), ft::make_pair(2, 1)));
+}
+
+TEST(map, key_comp_types)
+{
+	Map m;
+
+	const Map::key_compare &c1 = m.key_comp();
+	const Map::key_compare &c2 = m.key_comp();
+
+	EXPECT_NE(&c1, &c2);
+	EXPECT_EQ(typeid(m.key_comp()), typeid(std::less<ftc::Data>));
+}
+
+TEST(map, key_comp_types_const)
+{
+	const Map m;
+
+	const Map::key_compare &c1 = m.key_comp();
+	const Map::key_compare &c2 = m.key_comp();
+
+	EXPECT_NE(&c1, &c2);
+	EXPECT_EQ(typeid(m.key_comp()), typeid(std::less<ftc::Data>));
+}
+
+TEST(map, key_comp_types_rev)
+{
+	typedef ft::map<ftc::Data, ftc::Data, RevCmp<ftc::Data>, ftc::Allocator<ValueType> > Map2;
+
+	Map2 m;
+
+	const Map2::key_compare &c1 = m.key_comp();
+	const Map2::key_compare &c2 = m.key_comp();
+
+	EXPECT_NE(&c1, &c2);
+	EXPECT_EQ(typeid(m.key_comp()), typeid(RevCmp<ftc::Data>));
+}
+
+TEST(map, key_comp_types_const_rev)
+{
+	typedef ft::map<ftc::Data, ftc::Data, RevCmp<ftc::Data>, ftc::Allocator<ValueType> > Map2;
+
+	const Map2 m;
+
+	const Map2::key_compare &c1 = m.key_comp();
+	const Map2::key_compare &c2 = m.key_comp();
+
+	EXPECT_NE(&c1, &c2);
+	EXPECT_EQ(typeid(m.key_comp()), typeid(RevCmp<ftc::Data>));
+}
+
+TEST(map, key_comp)
+{
+	Map m;
+
+	EXPECT_TRUE(m.key_comp()(1, 2));
+	EXPECT_FALSE(m.key_comp()(2, 2));
+	EXPECT_FALSE(m.key_comp()(2, 1));
+}
+
+TEST(map, key_comp_const)
+{
+	Map m;
+
+	EXPECT_TRUE(m.key_comp()(1, 2));
+	EXPECT_FALSE(m.key_comp()(2, 2));
+	EXPECT_FALSE(m.key_comp()(2, 1));
+}
+
+TEST(map, key_comp_rev)
+{
+	typedef ft::map<ftc::Data, ftc::Data, RevCmp<ftc::Data>, ftc::Allocator<ValueType> > Map2;
+
+	Map2 m;
+
+	EXPECT_TRUE(m.key_comp()(2, 1));
+	EXPECT_FALSE(m.key_comp()(2, 2));
+	EXPECT_FALSE(m.key_comp()(1, 2));
+}
+
+TEST(map, key_comp_rev_const)
+{
+	typedef ft::map<ftc::Data, ftc::Data, RevCmp<ftc::Data>, ftc::Allocator<ValueType> > Map2;
+
+	const Map2 m;
+
+	EXPECT_TRUE(m.key_comp()(2, 1));
+	EXPECT_FALSE(m.key_comp()(2, 2));
+	EXPECT_FALSE(m.key_comp()(1, 2));
 }
