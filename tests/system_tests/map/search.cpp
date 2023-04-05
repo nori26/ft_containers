8152ba2a9a44295f691c8a568cb0fb9e58940f8b
nori <nori623p@gmail.com>
2023-04-06 01:24:00 +0200

    test: 💍 upper_bound

--- tests/system_tests/map/search.cpp
+++ tests/system_tests/map/search.cpp
@@ -81,3 +81,59 @@ TEST(map, lower_bound_empty_const)
 	EXPECT_EQ(m.lower_bound(-1), m.end());
 	EXPECT_EQ(m.lower_bound(INT_MIN), m.end());
 }
+
+TEST(map, upper_bound_types)
+{
+	const Map m;
+
+	EXPECT_EQ(typeid(Map().upper_bound(1)), typeid(Map::iterator));
+	EXPECT_EQ(typeid(m.upper_bound(1)), typeid(Map::const_iterator));
+}
+
+TEST(map, upper_bound)
+{
+	Map m;
+
+	m.insert(ft::make_pair(1, 1));
+	EXPECT_EQ(m.upper_bound(-1), m.begin());
+	EXPECT_EQ(m.upper_bound(0), m.begin());
+	EXPECT_EQ(m.upper_bound(1), m.end());
+	EXPECT_EQ(m.upper_bound(4), m.end());
+	EXPECT_EQ(m.upper_bound(5), m.end());
+}
+
+TEST(map, upper_bound2)
+{
+	Map m;
+
+	for (int i = 1; i < 4; i++) {
+		m.insert(ft::make_pair(i, i));
+	}
+	Map::iterator it = m.begin();
+	for (int i = 1; i < 4; ++i) {
+		EXPECT_EQ(m.upper_bound(i), ++it);
+	}
+	EXPECT_EQ(m.upper_bound(-1), m.begin());
+	EXPECT_EQ(m.upper_bound(0), m.begin());
+	EXPECT_EQ(m.upper_bound(4), m.end());
+	EXPECT_EQ(m.upper_bound(5), m.end());
+}
+
+TEST(map, upper_bound_empty)
+{
+	Map m;
+
+	EXPECT_EQ(m.upper_bound(0), m.end());
+	EXPECT_EQ(m.upper_bound(-1), m.end());
+	EXPECT_EQ(m.upper_bound(INT_MIN), m.end());
+}
+
+// TODO const
+TEST(map, upper_bound_empty_const)
+{
+	const Map m;
+
+	EXPECT_EQ(m.upper_bound(0), m.end());
+	EXPECT_EQ(m.upper_bound(-1), m.end());
+	EXPECT_EQ(m.upper_bound(INT_MIN), m.end());
+}
